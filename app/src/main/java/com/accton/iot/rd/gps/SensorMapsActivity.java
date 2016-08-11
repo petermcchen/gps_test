1059e6ecc13d459e22701195a5bd909226ac3815
mc_chen <mc_chen@accton.com>
2016-08-11 10:33:58 +0800

    navigate to sensor anchor and zoom

--- app/src/main/java/com/accton/iot/rd/gps/SensorMapsActivity.java
+++ app/src/main/java/com/accton/iot/rd/gps/SensorMapsActivity.java
@@ -8,13 +8,34 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.HashMap;
 
 public class SensorMapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private final static String TAG = "SensorMapsActivity";
     private final static boolean DEBUG = true;
     private GoogleMap mMap;
+
+    private Circle mCircle1U = null;
+    private Circle mCircle2U = null;
+    private Circle mCircle3U = null;
+    private Circle mCircle4U = null;
+    private int mUnit = 500;
+    final LatLng ACCTON_TAIPEI_OFFICE = new LatLng(25.043468, 121.557564);
+    final LatLng ACCTON_Hsinchu_HQ = new LatLng(24.778367, 120.988137);
+    private LatLng mGWPoistion = ACCTON_TAIPEI_OFFICE;
+    private LatLng mCurrentPosition = null;
+    private MarkerOptions mPseudoMarkerOpt = null;
+    private HashMap<String, Marker> mPseudoMarkerDict = null;
+    private MarkerOptions mSensorMarkerOpt = null;
+    private Boolean mIsMapReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +66,99 @@ public class SensorMapsActivity extends FragmentActivity implements OnMapReadyCa
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+                mCircle1U.remove();
+                mCircle2U.remove();
+                mCircle3U.remove();
+                mCircle4U.remove();
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                mGWPoistion = marker.getPosition();
+                updateRing(mGWPoistion);
+            }
+        });
+
+        //Marker Iot Gateway
+        MarkerOptions markerGW = new MarkerOptions();
+        markerGW.position(mGWPoistion);
+        markerGW.title("IoT Gateway");
+        markerGW.draggable(true);
+
+        mMap.addMarker(markerGW);
+
+        updateRing(mGWPoistion);
+        mCurrentPosition = mGWPoistion;
+
+        mPseudoMarkerOpt = new MarkerOptions();
+        mPseudoMarkerOpt.position(mCurrentPosition);
+        mPseudoMarkerOpt.draggable(false);
+        mPseudoMarkerOpt.visible(false);
+        mPseudoMarkerOpt.anchor(0.5f, 0.5f);
+        mPseudoMarkerOpt.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+        mSensorMarkerOpt = new MarkerOptions();
+        mSensorMarkerOpt.position(mCurrentPosition);
+        mSensorMarkerOpt.draggable(false);
+        mSensorMarkerOpt.visible(false);
+        mSensorMarkerOpt.anchor(0.5f, 0.5f);
+        mSensorMarkerOpt.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+        MarkerOptions pseudoMarkerOpt = new MarkerOptions();
+        pseudoMarkerOpt.position(mCurrentPosition);
+        pseudoMarkerOpt.draggable(false);
+        pseudoMarkerOpt.visible(false);
+        pseudoMarkerOpt.anchor(0.5f, 0.5f);
+        pseudoMarkerOpt.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+
+        //for(int index = 0; index < 8; index++)
+        //    mPseudoMarkerDict.put(""+index, mMap.addMarker(pseudoMarkerOpt));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mCurrentPosition, 15)); // The desired zoom level, in the range of 2.0 to 21.0.
+        mIsMapReady = true;
+        //
+    }
+
+    private void updateRing(LatLng center) {
+        mCircle1U = drawCircleROI(center, mUnit , 0x08AA00FF);
+        mCircle2U = drawCircleROI(center, mUnit * 2, 0x08FF00FF);
+        mCircle3U = drawCircleROI(center, mUnit * 3, 0x08FF00AA);
+        mCircle4U = drawCircleROI(center, mUnit * 4, 0x08FFAAFF);
+    }
+
+    private void drawSquareROI(LatLng center, double sideLength, int roiColor) {
+        // Polylines are useful for marking paths and routes on the map.
+        mMap.addPolyline(new PolylineOptions().geodesic(true).color(roiColor)
+                .add(new LatLng(center.latitude - sideLength, center.longitude - sideLength))
+                .add(new LatLng(center.latitude + sideLength, center.longitude - sideLength))
+                .add(new LatLng(center.latitude + sideLength, center.longitude + sideLength))
+                .add(new LatLng(center.latitude - sideLength, center.longitude + sideLength))
+                .add(new LatLng(center.latitude - sideLength, center.longitude - sideLength))
+        );
+    }
+
+    private Circle drawCircleROI(LatLng center, double radius, int roiColor) {
+        // Instantiates a new CircleOptions object and defines the center and radius
+        CircleOptions circleOptions = new CircleOptions()
+                .strokeWidth((float)(1.0))
+                .fillColor(roiColor)
+                .center(center)
+                .radius(radius); // In meters
+
+        // Get back the mutable Circle
+        return mMap.addCircle(circleOptions);
     }
 }
